e59dd78bc6c4633173cd984eb337a2d418cc894a
pavel <mrpavel23ooo@mail.ru>
2024-01-24 03:59:36 +1000

    version 3

--- account/authentication.py
+++ account/authentication.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import User
+from .models import Profile
 
 
 class EmailAuthBackend:
@@ -20,3 +21,10 @@ class EmailAuthBackend:
             return User.objects.get(pk=user_id)
         except User.DoesNotExist:
             return None
+
+
+def create_profile(backend, user, *args, **kwargs):
+    """
+    Создать профиль пользователя для социальной аутентификации
+    """
+    Profile.objects.get_or_create(user=user)
