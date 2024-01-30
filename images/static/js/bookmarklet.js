b95f89fe8ef71acd0fec3270c5ade567c8143dcb
pavel <mrpavel23ooo@mail.ru>
2024-01-31 03:33:27 +1000

    version 6

--- images/static/js/bookmarklet.js
+++ images/static/js/bookmarklet.js
@@ -3,15 +3,15 @@ const styleUrl = siteUrl + 'static/css/bookmarklet.css';
 const minWidth = 250;
 const minHeight = 250;
 
-// загрузить CSS
-var head = document.getElementsByTagName('head')[0];
-var link = document.createElement('link');
-link.rel = 'stylesheet';
+// load CSS
+var head = document.getElementsByTagName('head')[0];  // Get HTML head element
+var link = document.createElement('link'); // Create new link Element
+link.rel = 'stylesheet'; // set the attributes for link element
 link.type = 'text/css';
-link.href = styleUrl + '?r=' + Math.floor(Math.random() * 9999999999999999);
-head.appendChild(link);
+link.href = styleUrl + '?r=' + Math.floor(Math.random()*9999999999999999);
+head.appendChild(link);  // Append link element to HTML head
 
-// загрузить HTML
+// load HTML
 var body = document.getElementsByTagName('body')[0];
 boxHtml = `
   <div id="bookmarklet">
@@ -22,43 +22,45 @@ boxHtml = `
 body.innerHTML += boxHtml;
 
 function bookmarkletLaunch() {
-    bookmarklet = document.getElementById('bookmarklet');
-    var imagesFound = bookmarklet.querySelector('.images');
-    // очистить найденные изображения
-    imagesFound.innerHTML = '';
-    // показать букмарклет
-    bookmarklet.style.display = 'block';
+  bookmarklet = document.getElementById('bookmarklet');
+  var imagesFound = bookmarklet.querySelector('.images');
 
-    // событие закрытия
-    bookmarklet.querySelector('#close')
-        .addEventListener('click', function () {
-            bookmarklet.style.display = 'none'
-        });
+  // clear images found
+  imagesFound.innerHTML = '';
+  // display bookmarklet
+  bookmarklet.style.display = 'block';
 
-    // найти изображения в DOM с минимальными размерами
-    images = document.querySelectorAll('img[src$=".jpg"], img[src$=".jpeg"], img[src$=".png"]');
-    images.forEach(image => {
-        if (image.naturalWidth >= minWidth
-            && image.naturalHeight >= minHeight) {
-            var imageFound = document.createElement('img');
-            imageFound.src = image.src;
-            imagesFound.append(imageFound);
-        }
+  // close event
+  bookmarklet.querySelector('#close')
+             .addEventListener('click', function(){
+    bookmarklet.style.display = 'none'
+  });
+
+  // find images in the DOM with the minimum dimensions
+  images = document.querySelectorAll('img[src$=".jpg"], img[src$=".jpeg"], img[src$=".png"]');
+  images.forEach(image => {
+    if(image.naturalWidth >= minWidth
+       && image.naturalHeight >= minHeight)
+    {
+      var imageFound = document.createElement('img');
+      imageFound.src = image.src;
+      imagesFound.append(imageFound);
+    }
+  })
+
+  // select image event
+  imagesFound.querySelectorAll('img').forEach(image => {
+    image.addEventListener('click', function(event){
+      imageSelected = event.target;
+      bookmarklet.style.display = 'none';
+      window.open(siteUrl + 'images/create/?url='
+                  + encodeURIComponent(imageSelected.src)
+                  + '&title='
+                  + encodeURIComponent(document.title),
+                  '_blank');
     })
-    // событие выбора изображения
-    imagesFound.querySelectorAll('img').forEach(image => {
-        image.addEventListener('click', function (event) {
-            imageSelected = event.target;
-            bookmarklet.style.display = 'none';
-            window.open(siteUrl + 'images/create/?url='
-                + encodeURIComponent(imageSelected.src)
-                + '&title='
-                + encodeURIComponent(document.title),
-                '_blank');
-        })
-    })
-
+  })
 }
 
-// запустить букмарклет
+// launch the bookmkarklet
 bookmarkletLaunch();
--- images/views.py
+++ images/views.py
@@ -4,6 +4,12 @@ from django.contrib import messages
 from .forms import ImageCreateForm
 from django.shortcuts import get_object_or_404
 from .models import Image
+from django.http import JsonResponse
+from django.views.decorators.http import require_POST
+from django.http import HttpResponse
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.utils.text import slugify
+
 
 
 @login_required
@@ -36,3 +42,49 @@ def image_detail(request, id, slug):
                   'images/image/detail.html',
                   {'section': 'images',
                    'image': image})
+
+
+@login_required
+@require_POST
+def image_like(request):
+    image_id = request.POST.get('id')
+    action = request.POST.get('action')
+    if image_id and action:
+        try:
+            image = Image.objects.get(id=image_id)
+            if action == 'like':
+                image.users_like.add(request.user)
+            else:
+                image.users_like.remove(request.user)
+            return JsonResponse({'status': 'ok'})
+        except Image.DoesNotExist:
+            pass
+    return JsonResponse({'status': 'error'})
+
+
+@login_required
+def image_list(request):
+    images = Image.objects.all()
+    paginator = Paginator(images, 8)
+    page = request.GET.get('page')
+    images_only = request.GET.get('images_only')
+    try:
+        images = paginator.page(page)
+    except PageNotAnInteger:
+        # Если страница не является целым числом,то доставить первую страницу
+        images = paginator.page(1)
+    except EmptyPage:
+        if images_only:
+            # Если AJAX-запрос и страница вне диапазона, то вернуть пустую страницу
+            return HttpResponse('')
+        # Если страница вне диапазона, то вернуть последнюю страницу результатов
+        images = paginator.page(paginator.num_pages)
+    if images_only:
+        return render(request,
+                      'images/image/list_images.html',
+                      {'section': 'images',
+                       'images': images})
+    return render(request,
+                  'images/image/list.html',
+                  {'section': 'images',
+                   'images': images})
